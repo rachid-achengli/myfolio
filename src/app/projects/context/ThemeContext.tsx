93cdfb46d128ca9afdd81f644331f91a71aab7af
rachidachengli <rachid.achengli@edu.uiz.ac.ma>
2025-11-16 17:42:08 +0100

    Adding theme context

---
+++ src/app/projects/context/ThemeContext.tsx
@@ -0,0 +1,50 @@
+"Use client"
+import { createContext, useContext, useEffect, useState } from "react";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+  setTheme: (theme: Theme) => void;
+}
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+export const ThemeProvider = ({children}: {children: React.ReactNode}) => {
+    const [theme, setThemeState] = useState<Theme>("light");
+    const [mounted, setMounted] = useState(false);
+    const setTheme = (newTheme: Theme) => {
+        setThemeState(newTheme);
+        localStorage.setItem("theme", newTheme);
+        document.documentElement.classList.toggle("dark", newTheme === "dark");
+    }
+    //togle the theme
+    const toggleTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light");
+    }
+    //initialize theme 
+    useEffect(() => {
+        const savedTheme = localStorage.getItem("theme") as Theme | null;
+        const systemeTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        const initialTheme = savedTheme || systemeTheme;
+        setThemeState(initialTheme);
+        document.documentElement.classList.toggle("dark", initialTheme === "dark");
+        setMounted(true);
+    }, []);
+    //prevent flash of incorrect theme
+    if(!mounted){
+        return null;
+    }
+    return (
+        <ThemeContext.Provider value={{theme, setTheme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+//use custom hook to use theme context
+export function useTheme() {
+    const context = useContext(ThemeContext);
+    if (context === undefined) {
+        throw new Error("useTheme must be used within a ThemeProvider");
+    }
+    return context;
+}
