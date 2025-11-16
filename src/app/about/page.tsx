05f13d09258c097da48e5f089dbc017259d3349f
rachidachengli <rachid.achengli@edu.uiz.ac.ma>
2025-11-16 12:18:04 +0100

    Add blogs section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const page = () => {
-    return(
-        <div>Page</div>
-    )
+const About = () => {
+  return (
+    <div>About Page</div>
+  )
 }
-export default page;
+
+export default About
---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <div>Blogs page</div>
+  )
+}
+
+export default Blogs
