a39bbcf11f9feeef505510940596d0213b568976
rachidachengli <rachid.achengli@edu.uiz.ac.ma>
2025-11-18 15:16:44 +0100

    add blogs links

--- src/app/components/Blogs.tsx
+++ src/app/components/Blogs.tsx
@@ -1,8 +1,25 @@
+import { blogs } from '@/contents/blogs'
+import { article } from 'framer-motion/client'
+import Link from 'next/link'
 import React from 'react'
 
 const Blogs = () => {
   return (
-    <section className='py-20 container max-w-7xl mx-auto px-4'>Blogs</section>
+    <section className='py-20 container max-w-7xl mx-auto px-4'>
+        <h2 className='text-3xl font-bold mb-12 text-center'>Latest Blogs Posts</h2>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
+            {
+                blogs.map((blog) => (
+                    <article key={blog.slug} className='bg-white dark:bg-dark/50 rounded-lg shadow-md p-6'>
+                        <Link href={`/blogs/${blog.slug}`}>
+                        <h3 className='text-xl font-semibold mb-2 hover:text-primary transition-colors'>{blog.title}</h3>
+                        </Link>
+                    </article>
+                        
+                        ))
+            }
+        </div>
+    </section>
   )
 }
 
