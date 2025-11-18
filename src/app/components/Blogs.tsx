835173a4487905d89a050abc77478b7d03170871
rachidachengli <rachid.achengli@edu.uiz.ac.ma>
2025-11-18 14:22:13 +0100

    adding blogs section

---
+++ src/app/components/Blogs.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <section className='py-20 container max-w-7xl mx-auto px-4'>Blogs</section>
+  )
+}
+
+export default Blogs
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Blogs from "./components/Blogs";
 
 export default function Home() {
   return (
     <>
       <Hero/>
       <Projects />
+      <Blogs />
     </>
   );
 }
