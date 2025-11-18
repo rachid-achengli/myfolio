2b5b56c6d0a6ec7716e27d3560de75eeb63049d2
rachidachengli <rachid.achengli@edu.uiz.ac.ma>
2025-11-18 16:03:26 +0100

    add newslatter section

---
+++ src/app/components/Newsletter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Newsletter = () => {
+  return (
+    <section className='py-20 bg-white dark:bg-dark/50 rounded-lg shadow-md overflow-hidden animate-slideUp'>
+        <div>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Newsletter
