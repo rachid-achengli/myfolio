dc2b6bd7f4c7e07593667d2e3902ab5b6316074d
rachid-achengli <rachidachengli72@gmail.com>
2025-12-29 12:33:49 +0100

    Adding subscribe labels and paragraph

--- src/app/components/Newsletter.tsx
+++ src/app/components/Newsletter.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <section className='py-20 bg-white dark:bg-dark/50 rounded-lg shadow-md overflow-hidden animate-slideUp'>
-        <div>
-            
+    <section className='py-20 bg-white dark:bg-dark/50 rounded-lg shadow-md overflow-hidden animate-slide-up'>
+        <div className='p-8 md:p-12'>
+            <div className='max-w-7xl mx-auto'>
+              <h2 className='text-2xl font-bold mb-4'>Subscribe to My Newsletter</h2>
+              <p className='text-gray-600 dark:text-gray-300 md:w-2/3'>Stay updated with the latest blog posts and projects. 
+              Subscribe to my newsletter and never miss out!</p> 
+            </div>
         </div>
     </section>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Blogs from "./components/Blogs";
+import Newsletter from "./components/Newsletter";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero/>
       <Projects />
       <Blogs />
+      <Newsletter />
     </>
   );
 }
